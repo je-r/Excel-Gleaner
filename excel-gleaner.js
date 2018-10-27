b21c2addba577aa1562b2dfdda9e020ad3b63cae
je-r <5433476+je-r@users.noreply.github.com>
2018-10-27 18:27:57 +0200

    in vs code

--- excel-gleaner.js
+++ excel-gleaner.js
@@ -31,5 +31,44 @@ document.querySelector('.readBytesButtons').addEventListener('click', function(e
     }
 }, false);
 
-var version_excel_gleaner_js = 1;
+function showSheets()
+{
+    var files = document.getElementById('files').files;
+    if (!files.length) {
+        alert('Please select a file!');
+        return;
+    }
+    var file = files[0]; 
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        var data = e.target.result; // not working in chrome: result();
+        var workbook = XLSX.read(data, {
+          type: 'binary'
+        });
+  
+        workbook.SheetNames.forEach(function(sheetName) {
+          // Here is your object
+          //var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+          //var json_object = JSON.stringify(XL_row_object);
+          //console.log(json_object);
+          console.log("sheetName='"+sheetName+"'");
+        })
+  
+      }; // reader.onload
+  
+      reader.onerror = function(ex) {
+        console.log(ex);
+      };
+  
+      reader.readAsBinaryString(file);
+} // showSheets()
+
+
+document.querySelector('.excel').addEventListener('click', function(evt) {
+    if (evt.target.tagName.toLowerCase() == 'button') {
+        showSheets();
+    }
+}, false);
+
+var version_excel_gleaner_js = 3;
 
