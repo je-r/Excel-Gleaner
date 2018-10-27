4293d12e11ef604f2ee94d1c62bcce31ae55ba19
je-r <5433476+je-r@users.noreply.github.com>
2018-10-27 17:42:24 +0200

    in vs code

--- excel-gleaner.js
+++ excel-gleaner.js
@@ -1,34 +1,35 @@
 //
 function readBlob() {
-
-var files = document.getElementById('files').files;
-if (!files.length) {
-    alert('Please select a file!');
-    return;
-}
-
-var file = files[0];
-var start =  0;
-var stop = file.size - 1;
-
-var reader = new FileReader();
-
-// If we use onloadend, we need to check the readyState.
-reader.onloadend = function(evt) {
-    if (evt.target.readyState == FileReader.DONE) { // DONE == 2
-    // document.getElementById('byte_content').textContent = evt.target.result;
-    document.getElementById('byte_range').textContent = 
-        ['Read bytes: ', start + 1, ' - ', stop + 1,
-            ' of ', file.size, ' byte file'].join('');
+    var files = document.getElementById('files').files;
+    if (!files.length) {
+        alert('Please select a file!');
+        return;
     }
-};
-var blob = file.slice(start, stop + 1);
-reader.readAsBinaryString(blob);
-}
+
+    var file = files[0];
+    var start =  0;
+    var stop = file.size - 1;
+
+    var reader = new FileReader();
+
+    // If we use onloadend, we need to check the readyState.
+    reader.onloadend = function(evt) {
+        if (evt.target.readyState == FileReader.DONE) { // DONE == 2
+        // document.getElementById('byte_content').textContent = evt.target.result;
+        document.getElementById('byte_range').textContent = 
+            ['Read bytes: ', start + 1, ' - ', stop + 1,
+                ' of ', file.size, ' byte file'].join('');
+        }
+    };
+    var blob = file.slice(start, stop + 1);
+    reader.readAsBinaryString(blob);
+} // readBlob()
 
 document.querySelector('.readBytesButtons').addEventListener('click', function(evt) {
-if (evt.target.tagName.toLowerCase() == 'button') {
-    readBlob();
-}
+    if (evt.target.tagName.toLowerCase() == 'button') {
+        readBlob();
+    }
 }, false);
+
+var version_excel_gleaner_js = 1;
 
